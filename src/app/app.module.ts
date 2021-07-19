0500c5cd72a75f2579b75fbb1fc6a35e2ea62dc4
Gabriel Batista <gabrielmontilla04@gmail.com>
2021-07-19 14:07:05 -0400

    Agregamos nuestros componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componets/inicio/inicio.component';
+import { BienvenidaComponent } from './componet/inicio/bienvenida/bienvenida.component';
+import { LoginComponent } from './componet/inicio/login/login.component';
+import { RegistroComponent } from './componet/inicio/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    BienvenidaComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
