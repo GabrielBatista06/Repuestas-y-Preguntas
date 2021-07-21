8892d4a75594a892ecc230812f70f194120e9ee6
Gabriel Batista <gabrielmontilla04@gmail.com>
2021-07-21 16:48:02 -0400

    Seguimos diseñando la pagina de registrarse

--- src/app/componet/inicio/registro/registro.component.ts
+++ src/app/componet/inicio/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,10 +7,29 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
-  register!: FormGroup;
-  constructor() { }
+  register: FormGroup;
+
+  constructor(private fb: FormBuilder) { 
+    this.register = this.fb.group({
+      usuario: ['', Validators.required],
+      password: ['', [Validators.required,Validators.minLength(6)]],
+      confirmPassword: ['']
+
+    }, {Validator: this.checkPassword });
+  }
 
   ngOnInit(): void {
+  }
+
+  registrarUsuario():void{
+    console.log(this.register);
+  }
+
+  checkPassword(group: FormGroup): any {
+    const pass = group.controls.password.value;
+    const confirmPass = group.controls.confirmPassword.value;
+    return pass === confirmPass ? null : {notSame: true};
+
   }
 
 }
